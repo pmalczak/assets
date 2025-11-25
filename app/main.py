d0f2e657eaef06deb0450e5479e69f947e413951
PiotrMalczak <pmalczak@gmail.com>
2025-11-25 09:15:41 +0100

    marimo introduced

--- app/main.py
+++ app/main.py
@@ -17,11 +17,12 @@ from nbp_fx_repo.nbp_fx_repository import NbpFxRepository, NBP_API_EUR
 s = '________________________________________________\n'
 col_space=15
 
-def main():
+
+def calculate_assets() -> pd.DataFrame:
     local_data_steps_root = Path(__file__).parent.parent
     DATA_STEP.init_steps(root=local_data_steps_root)
 
-    DATA_STEP.force_read_data()
+    # DATA_STEP.force_read_data()
 
     data_root = get_online_data_root()
 
@@ -40,13 +41,18 @@ def main():
     assets = assets.sort_values(by=[AssetsFile.GROUP, AssetsFile.ID])
     assets = assets[assets[AssetsDef.VALUE] != 0]
     assets = assets.drop(columns=[AssetsDef.NOTES, LastFx.FX])
+    return assets
+
+
+def main():
+    assets = calculate_assets()
     print(assets)
     print(s)
     assets.to_excel('assets_evaluation.xlsx', index=False)
 
     rap3(assets)
     rap2(assets)
-    rap1(assets)
+    rap1_prn(assets)
     return
 
 
@@ -100,9 +106,49 @@ def rap2(assets):
     print(s)
 
 
-def rap1(assets):
-    msg = 'RAP 1___________________________________________'
+def rap1_prn(assets):
+    msg = 'RAP 1'
     print(msg)
+    g1 = rap1(assets)
+    print(g1.to_string(col_space=col_space))
+    # print(s)
+
+
+def rap1(assets: pd.DataFrame) -> pd.DataFrame:
+    a1 = assets[[AssetsDef.GROUP, AssetsDef.CURRENCY, AssetsDef.VALUE_PLN]]
+    a2 = a1.copy()
+    a2[AssetsDef.GROUP] = 'Z RAZEM'
+    a3 = a1.copy()
+    a3[AssetsDef.CURRENCY] = 'RAZEM'
+    a4 = a3.copy()
+    a4[AssetsDef.GROUP] = 'Z RAZEM'
+
+    # łączymy dane
+    df = pd.concat([a1, a2, a3, a4])
+
+    # grupowanie: GROUP + CURRENCY --> suma
+    g1 = df.groupby([AssetsDef.GROUP, AssetsDef.CURRENCY]).sum()
+
+    # pivot: waluty w kolumnach
+    g1 = g1.unstack(AssetsDef.CURRENCY).fillna(0)
+
+    # usuwamy MultiIndex kolumn po pivotowaniu
+    g1.columns = g1.columns.get_level_values(1)
+
+    # formatowanie kwot
+    for col in g1.columns:
+        g1[col] = (
+            g1[col]
+            .round()
+            .astype(int)
+            .map('{:,}'.format)
+            .str.replace(',', ' ')
+        )
+
+    return g1
+
+
+def rap1_0(assets):
     a1 = assets[[AssetsDef.GROUP, AssetsDef.VALUE_PLN]]
     a2 = a1.copy()
     a2[AssetsDef.GROUP] = 'Z RAZEM'
@@ -110,9 +156,7 @@ def rap1(assets):
     df = pd.concat([a1, a2])
     g1 = df.groupby([AssetsDef.GROUP]).sum().round().astype('int')
     g1[AssetsDef.VALUE_PLN] = g1[AssetsDef.VALUE_PLN].map('{:,}'.format).apply(lambda x: x.replace(',', ' '))
-    print(g1.to_string(col_space=col_space))
-    print(s)
-    return
+    return g1
 
 
 
