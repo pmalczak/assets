99dff663728f3d4ac37a81eca0dd735056ca996b
PiotrMalczak <pmalczak@gmail.com>
2025-11-27 23:17:54 +0100

    aquamarina @ consolidate

--- app/consolidate.py
+++ app/consolidate.py
@@ -32,18 +32,60 @@ def main():
 
         result += [df]
 
-    cleaned, report, meta = consolidate_many_drop_internal_transfers(result)
+    df, report, meta = consolidate_many_drop_internal_transfers(result)
+
+    df['Data operacji'] = pd.to_datetime(df['#Data operacji'], format='%Y-%m-%d')
+
+    df['ROK'] = df['Data operacji'].dt.year
+    df['MIESIAC'] = df['Data operacji'].dt.month
+    df['DZIEN'] = df['Data operacji'].dt.day
+
+    p = Path(__file__).parent
+    #
+    mask = (
+            df["#Nadawca/Odbiorca"].str.contains("AQUAMARINA") |
+            df["#Nadawca/Odbiorca"].str.contains("MIĘDZYZDROJE") |
+            df["#Nadawca/Odbiorca"].str.contains("MARINA INVEST") #
+    )
+    mask2 = df["#Numer konta"].str.contains("10124069600163204573190024")
+
+    df_aquamarina = df[mask | mask2].copy()
+    fout = p / f'mbank_aquamarina.xlsx'
+    df_aquamarina.to_excel(fout, index=False)
+    df_aquamarina = df_aquamarina[['ROK', '#Kwota', '#Opis operacji']]
+    total = df_aquamarina.copy()
+    total['#Opis operacji'] = 'TOTAL'
+    piv = pd.concat([df_aquamarina, total])
+    tabela = piv.pivot_table(
+        index='ROK',
+        columns='#Opis operacji',
+        values='#Kwota',
+        aggfunc='sum',
+        fill_value=0
+    )
+    tabela = tabela.round().astype('int').map('{:,}'.format).replace(',', ' ')
+
+    print(tabela.to_string(col_space=15))
+
+    df = df[~(mask | mask2)].copy()
+
     print(meta)
 
     # df = pd.concat([df1, df2])
-    p = Path(__file__).parent
     fout = p / f'mbank_consolidated.xlsx'
-    cleaned.to_excel(fout, index=False)
+    df.to_excel(fout, index=False)
 
     fout = p / f'mbank_consolidated.parquet'
-    cleaned.to_parquet(fout, compression=None)
+    df.to_parquet(fout, compression=None)
     return
 
 
 if __name__ == '__main__':
+    pd.options.mode.copy_on_write = True
+    pd.options.future.infer_string = True
+
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', 5000)
+    pd.set_option('display.colheader_justify', 'center')
     main()
